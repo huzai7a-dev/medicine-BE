2865c535f5ad4dbb6871f0a314f3bd409b9879ec
huzai7a-dev <shaikhhuzaifa072@gmail.com>
2023-11-17 19:39:56 -0500

    add env

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
--- src/helpers/Extrator.ts
+++ src/helpers/Extrator.ts
@@ -1,9 +1,11 @@
 import AWS from 'aws-sdk';
 
+const {ACCESS_KEY_ID,SECRET_ACCESS_KEY} = process.env;
+
 AWS.config.update({
   region:'ap-southeast-2',
-  accessKeyId: '',
-  secretAccessKey: ''
+  accessKeyId: ACCESS_KEY_ID,
+  secretAccessKey: SECRET_ACCESS_KEY
 });
 
 const textract = new AWS.Textract();
